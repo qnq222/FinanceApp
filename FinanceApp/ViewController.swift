4bd430f5859bb48a4526ec3d666f9c9117b678c5
Ayman <qnq222@gmail.com>
2020-05-27 14:22:36 +0300

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Ayman  on 5/27/20.
+//  Copyright © 2020 Ayman . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
